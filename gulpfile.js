b26666e9d41afdac84b3c0dbb13497828a4fe60f
misa5555 <misa.gk@icloud.com>
2016-01-31 15:57:53 -0800

    finished implementing event form

---
+++ gulpfile.js
@@ -0,0 +1,63 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var browserSync = require('browser-sync').create();
+var eslint = require('gulp-eslint');
+var jasmine = require('gulp-jasmine-phantom');
+var concat = require('gulp-concat');
+require('es6-promise').polyfill();
+
+gulp.task('default', ['styles', 'js', ], function() {
+	gulp.watch('sass/**/*.scss', ['styles']);
+	// gulp.watch('js/**/*.js', ['lint']);
+
+	browserSync.init({
+		server: './'
+	});
+});
+
+
+var paths = {
+  'select2': './node_modules/select2/dist/js/',
+  'bootstrap': '/node_modules/bootstrap/dist/js/'
+}
+gulp.task('js', function(){
+  gulp.src([
+    paths.select2 + 'select2.min.js',
+    paths.bootstrap + 'bootstrap.min.js'
+  ])
+  .pipe(concat("libs.js"))
+  .pipe(gulp.dest('dist/js'));
+});
+
+
+gulp.task('styles', function() {
+	gulp.src('sass/**/*.scss')
+		.pipe(sass().on('error', sass.logError))
+		.pipe(autoprefixer({
+			browsers: ['last 2 versions']
+		}))
+		.pipe(gulp.dest('./css'))
+		.pipe(browserSync.stream());
+});
+
+gulp.task('lint', function () {
+	return gulp.src(['js/**/*.js'])
+		// eslint() attaches the lint output to the eslint property
+		// of the file object so it can be used by other modules.
+		.pipe(eslint())
+		// eslint.format() outputs the lint results to the console.
+		// Alternatively use eslint.formatEach() (see Docs).
+		.pipe(eslint.format())
+		// To have the process exit with an error code (1) on
+		// lint error, return the stream and pipe to failOnError last.
+		.pipe(eslint.failOnError());
+});
+
+gulp.task('tests', function () {
+	gulp.src('tests/spec/extraSpec.js')
+		.pipe(jasmine({
+			integration: true,
+			vendor: 'js/**/*.js'
+		}));
+});
---
+++ js/main.js
@@ -0,0 +1,151 @@
+var eventsRef = new Firebase('https://sweltering-inferno-2083.firebaseio.com/')
+
+function checkValidation ( $el ) {
+    if($el[0].validationMessage !== ""){
+      $el.parent().addClass('has-error');
+      var className = $el.attr('id');
+      $('span.help-block.' + className).text($el[0].validationMessage)
+    }   
+}
+
+function checkGuestPresence(){
+  var guestsArray = $('#guests').val();
+  if(guestsArray === null ){
+    $('.guests').text("Please invite at least one guest.");
+  }
+}
+
+function checkLocation(){
+  var address = $('#pac-input').val();
+  if (address === ""){
+    $('.location').text("Please fill out address.");
+  }
+}
+$(function () {
+  var dateNow = new Date();
+  // $('#start_date, #end_date').datetimepicker({ defaultDate: dateNow});
+  // var dateTomorrow = dateNow.setDate(dateNow.getTime() + 60*60*1000); 
+  $('#start_date').datetimepicker({ minDate: dateNow, defaultDate: dateNow , stepping: 30, sideBySide: true}); 
+  $('#end_date').datetimepicker({ minDate: dateNow, defaultDate: dateNow, stepping: 30, sideBySide: true }); 
+  $("#guests").select2();
+
+  // google.maps.event.addDomListener(window, 'load', initMap);
+
+  // google.maps.event.addDomListener(window, 'load', initializeGoogleMap);
+
+  $('button#submit').on('click', function(event){
+    var name = $('input#eventName').val();
+    var type = $('input#eventType').val();
+    var host = $('input#eventHost').val();
+    var startDate = $('input#start_date').val();
+    var endDate = $('input#end_date').val();
+    var guests = $('#guests').val();
+    var location = $('#pac-input').val();
+    var memo = $('input#memo').val();
+
+    checkValidation($('input#eventName'));
+    checkValidation($('input#eventType'));
+    checkValidation($('input#eventHost'));
+    checkValidation($('input#start_date'));
+    checkValidation($('input#end_date'));
+    checkGuestPresence();
+    checkLocation();
+    eventsRef.push({ name: name, type: type, host: host, startDate: startDate, endDate: endDate, guests: guests, location: location, memo: memo})
+    // window.location.href = "/event_form"
+  });
+
+
+});
+function initAutocomplete() {
+  
+  var initialLocation;
+  var myOptions = {
+    zoom: 10,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+  var map = new google.maps.Map(document.getElementById("map"), myOptions);
+ // Try W3C Geolocation (Preferred)
+  if(navigator.geolocation) {
+    browserSupportFlag = true;
+    navigator.geolocation.getCurrentPosition(function(position) {
+      initialLocation = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
+      map.setCenter(initialLocation);
+    }, function() {
+      handleNoGeolocation(browserSupportFlag);
+    });
+  }
+  // Browser doesn't support Geolocation
+  else {
+    browserSupportFlag = false;
+    handleNoGeolocation(browserSupportFlag);
+  } 
+
+  // Create the search box and link it to the UI element.
+  var input = document.getElementById('pac-input');
+  var searchBox = new google.maps.places.SearchBox(input);
+  map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+  // Bias the SearchBox results towards current map's viewport.
+  map.addListener('bounds_changed', function() {
+    searchBox.setBounds(map.getBounds());
+  });
+
+  var markers = [];
+  // [START region_getplaces]
+  // Listen for the event fired when the user selects a prediction and retrieve
+  // more details for that place.
+  searchBox.addListener('places_changed', function() {
+    var places = searchBox.getPlaces();
+
+    if (places.length == 0) {
+      return;
+    }
+
+    // Clear out the old markers.
+    markers.forEach(function(marker) {
+      marker.setMap(null);
+    });
+    markers = [];
+
+    // For each place, get the icon, name and location.
+    var bounds = new google.maps.LatLngBounds();
+    places.forEach(function(place) {
+      var icon = {
+        url: place.icon,
+        size: new google.maps.Size(71, 71),
+        origin: new google.maps.Point(0, 0),
+        anchor: new google.maps.Point(17, 34),
+        scaledSize: new google.maps.Size(25, 25)
+      };
+
+      // Create a marker for each place.
+      markers.push(new google.maps.Marker({
+        map: map,
+        icon: icon,
+        title: place.name,
+        position: place.geometry.location
+      }));
+
+      if (place.geometry.viewport) {
+        // Only geocodes have viewport.
+        bounds.union(place.geometry.viewport);
+      } else {
+        bounds.extend(place.geometry.location);
+      }
+    });
+    map.fitBounds(bounds);
+
+  });
+
+  function handleNoGeolocation(errorFlag) {
+    if (errorFlag == true) {
+      alert("Geolocation service failed.");
+      initialLocation = newyork;
+    } else {
+      alert("Your browser doesn't support geolocation. We've placed you in Siberia.");
+      initialLocation = siberia;
+    }
+    map.setCenter(initialLocation);
+  }
+  // [END region_getplaces]
+}
