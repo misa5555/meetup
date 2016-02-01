21fcc2aabd8f6c1d8d42be0be51dbfef68c02c7b
misa5555 <misa.gk@icloud.com>
2016-01-31 21:18:33 -0800

    consolidate js/css into one file

--- gulpfile.js
+++ gulpfile.js
@@ -5,10 +5,12 @@ var browserSync = require('browser-sync').create();
 var eslint = require('gulp-eslint');
 var jasmine = require('gulp-jasmine-phantom');
 var concat = require('gulp-concat');
+var concatCss = require('gulp-concat-css');
 require('es6-promise').polyfill();
 
-gulp.task('default', ['styles', 'js', ], function() {
+gulp.task('default', ['styles', 'js', 'css'], function() {
 	gulp.watch('sass/**/*.scss', ['styles']);
+  gulp.watch('dist/js/*.js', ['js']);
 	// gulp.watch('js/**/*.js', ['lint']);
 
 	browserSync.init({
@@ -19,12 +21,16 @@ gulp.task('default', ['styles', 'js', ], function() {
 
 var paths = {
   'select2': './node_modules/select2/dist/js/',
-  'bootstrap': '/node_modules/bootstrap/dist/js/'
+  'bootstrap': './node_modules/bootstrap/dist/js/',
+  'datetimepicker': './bower_components/eonasdan-bootstrap-datetimepicker/build/js/',
+  'moment': './bower_components/moment/min/'
 }
 gulp.task('js', function(){
   gulp.src([
     paths.select2 + 'select2.min.js',
-    paths.bootstrap + 'bootstrap.min.js'
+    paths.bootstrap + 'bootstrap.min.js',
+    paths.moment + 'moment.min.js',
+    paths.datetimepicker + 'bootstrap-datetimepicker.min.js',
   ])
   .pipe(concat("libs.js"))
   .pipe(gulp.dest('dist/js'));
@@ -39,6 +45,22 @@ gulp.task('styles', function() {
 		}))
 		.pipe(gulp.dest('./css'))
 		.pipe(browserSync.stream());
+});
+
+var pathsCss = {
+  'select2' : './node_modules/select2/dist/css/',
+  'datetimepicker': './bower_components/eonasdan-bootstrap-datetimepicker/build/css/',
+  'bootstrap': './css/'
+
+}
+gulp.task('css', function() {
+  gulp.src([
+    pathsCss.select2 + 'select2.min.css',
+    pathsCss.datetimepicker + 'bootstrap-datetimepicker.min.css',
+    pathsCss.bootstrap + 'bootstrap.min.css'
+  ])
+  .pipe(concatCss("libs.css"))
+  .pipe(gulp.dest('dist/css'));
 });
 
 gulp.task('lint', function () {
